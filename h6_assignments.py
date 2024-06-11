b8d763db1801fd22cc34744767d056b4bc30b47c
Arie Twigt <twigt.arie@gmail.com>
2024-06-11 10:29:23 +0200

    added assignment for adding days'

--- h6_assignments.py
+++ h6_assignments.py
@@ -17,4 +17,22 @@ for name in names_list:
 #%%
 names_list_new
 
+# %% 2. 
+import datetime
+
+#%% define the date of today
+date_today = datetime.date.today()
+
+#%% specify the number of days to increase
+num_days = 1
+max_days = 10
+
+while num_days <= max_days:
+    new_date = date_today + datetime.timedelta(days=num_days)
+    new_day = new_date.strftime("%A")
+    print(new_day)
+
+    num_days += 1
+
+
 # %%
